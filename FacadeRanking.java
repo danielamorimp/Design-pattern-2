9ed3a49903b359522f8f54081cf8e593b26a7e95
Gabriel110 <gabrielcorreiadev@gmail.com>
2020-11-29 21:04:51 -0300

    Padrão facade

---
+++ FacadeRanking.java
@@ -0,0 +1,6 @@
+public class FacadeRanking {
+  private Ranking ra;
+  public void litarRanking(){
+    this.ra.getInstance();
+  }
+}
--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ public class Main {
   
   public static void main(String[] args) {
     Personagem p = new Personagem();
+    FacadeRanking f = new FacadeRanking();
     p.setTendencia(new MundoHard());
     p.getTedenciaMundo();
+    f.litarRanking();
   }
 }
